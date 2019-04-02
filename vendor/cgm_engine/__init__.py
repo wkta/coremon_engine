0264c9b8d19f1dfee287519eed4ed35eaf1e3bfb
Thomas Iwaszko <thomas@gaudia-tech.com>
2019-04-02 11:44:55 +0200

    minor improvement: default scr_size and window caption are given

--- vendor/cgm_engine/__init__.py
+++ vendor/cgm_engine/__init__.py
@@ -5,7 +5,6 @@ designed & coded by Thomas Iwaszko
 contact: thomas@gaudia-tech.com
 (c) 2019
 """
-
 import pygame
 from pygame import constants
 
@@ -13,13 +12,23 @@ from ._defs import EngineEvTypes, enum_for_custom_event_types
 from ._events import EventManager, EventReceiver, CgmEvent, EventDispatcher, CogObject
 from .structures import BaseGameState
 
+
 PygameBridge = constants
 screen = None
+DEFAULT_CAPTION = 'Made with coremonger engine'
+DEFAULT_SCREEN_SIZE = (1024, 768)
 
 
-def init(scr_size=None):
-    global screen
+def init(scr_size=None, caption=None):
     pygame.init()
-    default_scr_size = (640, 480)
-    arg = default_scr_size if (scr_size is None) else scr_size
-    screen = pygame.display.set_mode(arg)
+
+    global screen
+    if scr_size:
+        screen = pygame.display.set_mode(scr_size)
+    else:
+        screen = pygame.display.set_mode(DEFAULT_SCREEN_SIZE)
+
+    if caption:
+        pygame.display.set_caption(caption)
+    else:
+        pygame.display.set_caption(DEFAULT_CAPTION)
